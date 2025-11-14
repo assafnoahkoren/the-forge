7a1c543bc537e830fdbe0d553313825f690570aa
Assaf Noah Koren <assaf.koren@connecteam.com>
2025-11-14 06:10:08 +0200

    .

--- apps/server/eslint.config.js
+++
@@ -1,7 +0,0 @@
-import { config as baseConfig } from '@repo/eslint-config/base'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  ...baseConfig,
-])
